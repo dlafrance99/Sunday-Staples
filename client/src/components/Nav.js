305d3d1d4ef0806df28c21337bd05f9e2211b35f
Emma Weber <emma.weber327@gmail.com>
2019-11-11 13:34:04 -0700

    committing client side logout button

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,6 +1,9 @@
-import React from "react"
-import { NavLink} from "react-router-dom"
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom"
 import styled from "styled-components"
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { logoutUser } from "../actions/authActions";
 
 const NavWrapper = styled.div`
 background-color: #d9d9d9;
@@ -26,67 +29,99 @@ h1 {
 `
 // I think we should make our nav bar with dropdowns so that Search can include Recipe and Nutrition, Profile could include Login, Register, Saved Recipes and Shopping list and Reviews
 // ex. Sunday Staples                   Home | Search | Profile | Reviews
-const Nav = () => {
-    return(
+
+
+class Nav extends Component {
+    onLogoutClick = e => {
+      e.preventDefault();
+      this.props.logoutUser();
+    };
+  render() {
+    return (
         <>
             <NavWrapper>
                 <h1>Sunday Staples</h1>
                 <NavLink
-                className="nav-link"
-                to="/home"
-                activeClassName="active"
-                isActive = {()=> window.location.pathname === "/home" || window.location.pathname === "home"}>
+                    className="nav-link"
+                    to="/home"
+                    activeClassName="active"
+                    isActive={() => window.location.pathname === "/home" || window.location.pathname === "home"}>
                     Home
                 </NavLink>
 
                 <span className="nav-link">|</span>
-                
+
                 <NavLink
-                className="nav-link"
-                to="/recipe-search"
-                activeClassName="active">
+                    className="nav-link"
+                    to="/recipe-search"
+                    activeClassName="active">
                     Recipe Search
                 </NavLink>
 
                 <span className="nav-link">|</span>
 
                 <NavLink
-                className="nav-link"
-                to="/nutrition-search"
-                activeClassName="active">
+                    className="nav-link"
+                    to="/nutrition-search"
+                    activeClassName="active">
                     Nutrition Search
                 </NavLink>
 
                 <span className="nav-link">|</span>
 
                 <NavLink
-                className="nav-link"
-                to="/saved-recipes"
-                activeClassName="active">
+                    className="nav-link"
+                    to="/saved-recipes"
+                    activeClassName="active">
                     Saved Recipes
                 </NavLink>
 
                 <span className="nav-link">|</span>
 
                 <NavLink
-                className="nav-link"
-                to="/shopping-list"
-                activeClassName="active">
+                    className="nav-link"
+                    to="/shopping-list"
+                    activeClassName="active">
                     Shopping List
                 </NavLink>
 
                 <span className="nav-link">|</span>
 
                 <NavLink
-                className="nav-link"
-                to="/reviews"
-                activeClassName="active">
+                    className="nav-link"
+                    to="/reviews"
+                    activeClassName="active">
                     Reviews
                 </NavLink>
+
+                <button
+                    style={{
+                        width: "100px",
+                        borderRadius: "3px",
+                        letterSpacing: "1.5px",
+                        marginTop: "1rem"
+                    }}
+                    onClick= {this.onLogoutClick}
+                    className="btn btn-large waves-effect waves-light hoverable blue accent-3 nav-link"
+                >
+                    Logout
+                </button>
 
             </NavWrapper>
         </>
     )
 }
+}
 
-export default Nav;
+Nav.propTypes = {
+    logoutUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired
+  };
+  const mapStateToProps = state => ({
+    auth: state.auth
+  });
+
+export default connect(
+    mapStateToProps,
+    { logoutUser }
+  )(Nav);
