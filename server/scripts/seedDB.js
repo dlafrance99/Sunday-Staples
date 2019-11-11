3c0402bdd65077ca6d0d5777d83524572f174934
Emma Weber <emma.weber327@gmail.com>
2019-11-11 14:14:46 -0700

    added data for featured recipes

--- server/scripts/seedDB.js
+++ server/scripts/seedDB.js
@@ -23,6 +23,22 @@ const featuredRecipeSeed = [
         totalTime: 160,
         servings: 12,
         ingredients: ['1/4 cup vegetable oil or melted unsalted butter, plus more for baking sheet', '1/4 cup ground cinnamon', 'Cinnamon Roll Dough, risen and at room temperature']
+    },
+    {
+        title: 'Strawberry Sundae With Strawberry Sauce',
+        url: 'http://www.bbcgoodfood.com/recipes/6317/',
+        image: 'https://www.edamam.com/web-img/2b4/2b4c16a6d5a606c052144947114276c5.jpg',
+        totalTime: 25,
+        servings: 6,
+        ingredients: ['3 tbsp golden caster sugar','4 tbsp orange juice','18 strawberries,hulled','6 small bananas','strawberry, vanilla and chocolate ice cream','284ml pot double cream','50g pecans,toasted and chopped']
+    },
+    {
+        title: 'Bbq Chicken Quinoa Salad',
+        url: 'http://whatsgabycooking.com/bbq-chicken-quinoa-salad/#.UlhzMVAy0uM',
+        image: 'https://www.edamam.com/web-img/40d/40d954a4c9f1cc2e0a85d64adc63fa85.jpg',
+        totalTime: 15,
+        servings: 1,
+        ingredients: ['1 1/2 cups cooked quinoa', '1/2 cup fresh corn cut from the cob', '1/2 cup black beans rinsed and drained', '1 cup shredded BBQ chicken can use a store bought chicken and tossed with a little BBQ sauce','1 avocado chopped','3-4 tablespoons White Cheddar cheese shredded', 'BBQ sauce', '2 scallions chopped', 'cilantro for garnish']
     }
 ];
 
