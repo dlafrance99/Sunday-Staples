e196401e2aae442ce39705ad876ddfd8f63152db
Devon LaFrance <lafrance.devon@gmail.com>
2019-11-12 13:09:29 -0700

    updated saved page to show the saved recipes

--- client/src/pages/SavedRecipes.js
+++ client/src/pages/SavedRecipes.js
@@ -1,11 +1,54 @@
-import React from "react"
+import React, { Component } from "react"
+import API from "../utils/API"
+import RecipeCard from "../components/informationCards/RecipeCard"
+import styled from "styled-components"
 
-const SavedRecipes = () => {
-    return(
-        <>
-            <h2>This is saved Recipes</h2>
-        </>
-    )
+const savedStyle = styled.div`
+.searchedRecipes{
+    width: 80vw;
+    margin-left: 10vw;
+    margin-top: 10px;
+}
+`
+
+class SavedRecipes extends Component {
+    state = {
+        savedRecipes: []
+    }
+
+    componentDidMount() {
+        this.getSaved();
+    }
+
+    getSaved = () => {
+        API.loadSaved()
+            .then(res => this.setState({ savedRecipes: res.data }))
+            .catch(err => console.log(err))
+    }
+
+    render() {
+        return (
+            <>
+                <savedStyle>
+                    <div className="searchedRecipes">
+                        <ul>
+                            {this.state.savedRecipes.map(recipe => (
+                                <RecipeCard
+                                    key={recipe.url}
+                                    name={recipe.title}
+                                    link={recipe.url}
+                                    image={recipe.image}
+                                    time={recipe.totalTime}
+                                    servings={recipe.servings}
+                                    ingredients={recipe.ingredients}
+                                />
+                            ))}
+                        </ul>
+                    </div>
+                </savedStyle>
+            </>
+        )
+    }
 }
 
 export default SavedRecipes;
